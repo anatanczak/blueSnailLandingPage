041f380ea58ab150d9b75bcfbd106833da2edc30
80010-84-08 <80010-84-08@exchange.ad.afpanet>
2020-01-21 16:49:37 +0100

    comment out print sttement  which causes problesm

--- js/script.js
+++ js/script.js
@@ -3,13 +3,14 @@ document.addEventListener('scroll', () => {
     const pixelsScrolled = window.scrollY || pageYOffset;
    // const fromBottom = document.getElementById('main').getBoundingClientRect().height-fromTop;
     // const downArrow = document.getElementById('downArrow');
-  
+  const bgpic = document.getElementsByClassName('background-image-1')
     if (pixelsScrolled > 10) { 
-        print("we are scrolling upwards")
+       // print("we are scrolling upwards")
     //   upArrow.style.display = 'block';
     } else {
+        bgpic.style.display = none
     //   upArrow.style.display = 'none';
-    print("we are scrolling downwards")
+    //print("we are scrolling downwards")
     }
   
 
