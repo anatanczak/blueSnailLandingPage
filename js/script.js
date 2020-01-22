52290d86d9135ecd73a96121d6b09aef310c7c0a
80010-84-08 <80010-84-08@exchange.ad.afpanet>
2020-01-22 13:34:05 +0100

    add logic wich works for the appearing and dissapearing arrow but there is no  arrow yet just the consol log

--- js/script.js
+++ js/script.js
@@ -1,30 +1,15 @@
+const body = document.body;
+let lastScroll = 0;
 
-
-document.addEventListener('scroll', () => {
-
-    const pixelsScrolled = window.scrollY || pageYOffset;
-    //TO DO: 1. Find out how we can get the initial rect that we can see in view point
-  // const fromBottom = document.getElementById('main').getBoundingClientRect().height-fromTop;
-    // const downArrow = document.getElementById('downArrow');
-  const bgpic = document.getElementsByClassName('background-image-1')
-    if (pixelsScrolled > 10) {
-      console.log("we are scrolling downwards");
-      //console.log(fromBottom)
-     // console.log(pixelsScrolled)
-    //   upArrow.style.display = 'block';
-    } else {
-        //bgpic.style.display = none
-    //   upArrow.style.display = 'none';
-    //print("we are scrolling downwards")
-    }
-    //kdhflkshhslfhslfhslkhflshlfhs
-
-
-
-    // if (pixelsScrolled > fromTop) {
-    // //   downArrow.style.display = 'block';
-    // } else {
-    // //   downArrow.style.display = 'none';
-    // }
+window.addEventListener('scroll', () => {
+  
+  const currentScroll = window.pageYOffset;
+  if (currentScroll == 0 || currentScroll < 10) {
+    console.log("we are not scrolling")
+    return;
+  }else{
+    console.log(currentScroll)
+    return;
+  }
 
   });
