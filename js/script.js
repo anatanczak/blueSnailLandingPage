2882042bd82ad62fad4116076fd2e0b1f22b4cbb
Ana Viktoriv <anaviktoriv@gmail.com>
2020-01-22 19:03:40 +0100

    add semicolon

--- js/script.js
+++ js/script.js
@@ -6,11 +6,11 @@ document.addEventListener('scroll', () => {
     //TO DO: 1. Find out how we can get the initial rect that we can see in view point
   // const fromBottom = document.getElementById('main').getBoundingClientRect().height-fromTop;
     // const downArrow = document.getElementById('downArrow');
-  const bgpic = document.getElementsByClassName('background-image-1')
+  const bgpic = document.getElementsByClassName('background-image-1');
     if (pixelsScrolled > 10) {
       console.log("we are scrolling downwards");
       //console.log(fromBottom)
-      console.log(pixelsScrolled)
+      console.log(pixelsScrolled);
     //   upArrow.style.display = 'block';
     } else {
         //bgpic.style.display = none
