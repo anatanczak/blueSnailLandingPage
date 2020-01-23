c11edd304346d5176ff2ab4be0b2ade3022d93ea
Ana Viktoriv <anaviktoriv@gmail.com>
2020-01-23 21:08:07 +0100

    add the animation to the arrow by toggling the css class

--- js/script.js
+++ js/script.js
@@ -1,15 +1,18 @@
 //jshint esversion: 6
 const body = document.body;
 let lastScroll = 0;
-
+var errow = document.getElementById('animated-arrow');
 window.addEventListener('scroll', () => {
 
   const currentScroll = window.pageYOffset;
   if (currentScroll == 0 || currentScroll < 10) {
-    console.log("we are not scrolling");
+//TO DO see if we can toggle class with classList.toggle
+    errow.classList.remove("hidden-arrow");
+    errow.classList.add('visible-arrow');
     return;
   }else{
-    console.log(currentScroll);
-    return;
+    errow.classList.remove('visible-arrow');
+    errow.classList.add("hidden-arrow");
+
   }
   });
