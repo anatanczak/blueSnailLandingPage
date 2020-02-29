6876fb79f03aa7112b9f6ee8190881b3558d1730
Ana Viktoriv <anaviktoriv@gmail.com>
2020-02-29 20:21:05 +0100

    finish front without prefixes

--- js/script.js
+++ js/script.js
@@ -1,18 +1,16 @@
 //jshint esversion: 6
 const body = document.body;
 let lastScroll = 0;
-var errow = document.getElementById('animated-arrow');
+var arrow = document.getElementById('animated-arrow');
 window.addEventListener('scroll', () => {
 
   const currentScroll = window.pageYOffset;
   if (currentScroll == 0 || currentScroll < 10) {
 //TO DO see if we can toggle class with classList.toggle
-    errow.classList.remove("hidden-arrow");
-    errow.classList.add('visible-arrow');
+    arrow.classList.remove("hidden-arrow");
     return;
   }else{
-    errow.classList.remove('visible-arrow');
-    errow.classList.add("hidden-arrow");
+    arrow.classList.add("hidden-arrow");
 
   }
   });
